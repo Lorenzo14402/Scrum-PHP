e2ffeb668ca2f4e9bf63edfde2b9e20868355497
Lorenzo14402 <lorenzo.hagen@student.graafschapcollege.nl>
2019-05-15 11:11:11 +0200

    Fase 1 van Bestelling.php en to-do voor mezelf.

---
+++ Lorenzo/Bestelling.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loren
+ * Date: 5/15/2019
+ * Time: 11:06 AM
+ */
+
+class Bestelling{
+    var $bestelNummer;
+    var $artikelFoto;
+    var $artikelNaam;
+    var $factuurNr;
+    var $bestelDatum;
+    var $bestelBedrag;
+    var $betaald; //true/false;
+
+    public function __construct()
+    {
+        
+    }
+}
+
+?>
