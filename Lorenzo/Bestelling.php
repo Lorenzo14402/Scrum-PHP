a69f86f712fa727e26c37c0a5f8d0335fe7345e4
Lorenzo14402 <lorenzo.hagen@student.graafschapcollege.nl>
2019-05-17 08:50:49 +0200

    Bestellingen.php done.
    Bestellingen.php is klaar + een css file toegevoegd voor de opmaak van een Bestelling object. Ook een simpele DB file gemaakt voor efficienter werk met een database connectie.

--- Lorenzo/Bestelling.php
+++ Lorenzo/Bestelling.php
@@ -6,19 +6,111 @@
  * Time: 11:06 AM
  */
 
+require_once "DB.php";
+
 class Bestelling{
     var $bestelNummer;
     var $artikelFoto;
+    var $bezorgingStatus;
     var $artikelNaam;
     var $factuurNr;
     var $bestelDatum;
     var $bestelBedrag;
     var $betaald; //true/false;
 
-    public function __construct()
+    public function __construct($bn)
     {
-        
+        $conf = new DB();
+        $conn = new mysqli($conf->dbhost, $conf->dbuser, $conf->dbpass, $conf->dbname);
+
+        if($bn != NULL) {
+            $query = "SELECT * FROM bestellingen WHERE bestelNummer='{$bn}'";
+            $result = $conn->query($query) or die($conn->error);
+
+            $bestelling = $result->fetch_assoc();
+
+            $this->bestelNummer = $bestelling['bestelNummer'];
+            $this->artikelFoto = $bestelling['artikelFoto'];
+            $this->bezorgingStatus = $bestelling['bezorgingStatus'];
+            $this->artikelNaam = $bestelling['artikelNaam'];
+            $this->factuurNr = $bestelling['factuurNr'];
+            $this->bestelDatum = $bestelling['bestelDatum'];
+            $this->bestelBedrag = $bestelling['bestelBedrag'];
+
+            $this->makeObject();
+
+            //$this->betaald = $bestelling['betaald'];
+        }else {//for null
+            $this->bestelNummer = NULL;
+            $this->artikelFoto = NULL;
+            $this->bezorgingStatus = NULL;
+            $this->artikelNaam = NULL;
+            $this->factuurNr = NULL;
+            $this->bestelDatum = NULL;
+            $this->bestelBedrag = NULL;
+            $this->betaald = NULL;
+
+            echo "Geen bestelNummer meegegeven.";
+        }
+    }
+
+    public function makeObject()
+    {
+        ?>
+        <head>
+        <link href="bestellingcss.css" rel="stylesheet" type="text/css">
+        </head>
+        <body>
+            <div id="bestelling">
+                <div id="items">
+                    <img src="<?php echo $this->artikelFoto?>">
+                    <table>
+                        <tr>
+                            <th style="font-size: 20px; padding: 0; text-align: left"><?php echo $this->bestelNummer?></th>
+                        </tr>
+                        <tr>
+                            <td style="padding: 10px;"></td>
+                        </tr>
+                        <tr>
+                            <th>Artikel naam</th>
+                            <th>Factuur nummer</th>
+                            <th>Bestel datum</th>
+                            <th>Bestel bedrag</th>
+                            <th>Betaald</th>
+                        </tr>
+                        <tr>
+                            <td><?php echo $this->artikelNaam?></td>
+                            <td><?php echo $this->factuurNr?></td>
+                            <td><?php echo $this->bestelDatum?></td>
+                            <td>€<?php echo $this->bestelBedrag?></td>
+                            <td><?php echo $this->betaald?></td>
+                        </tr>
+                    </table>
+                    <!--<img src="<?php /*echo $this->artikelFoto*/?>">
+                    <h3><?php /*echo $this->bestelNummer*/?></h3>
+                    <br>
+                    <strong>Artikel naam</strong>
+                    <strong>Factuur nummer</strong>
+                    <strong>Bestel datum</strong>
+                    <strong>Bestel bedrag</strong>
+                    <strong>Betaald</strong>
+                    <a><?php /*echo $this->artikelNaam*/?></a>
+                    <a><?php /*echo $this->factuurNr*/?></a>
+
+                    <a><?php /*echo $this->bestelDatum*/?></a>
+                    <a>€<?php /*echo $this->bestelBedrag*/?></a>
+                    <a><?php /*echo $this->betaald*/?></a>-->
+                </div>
+            </div>
+        </body>
+        <?php
     }
 }
+
+$test = new Bestelling("BEST123");
+
+$test2 = new Bestelling("BEST123");
+$test2 = new Bestelling("BEST123");
+
 
 ?>
---
+++ Lorenzo/DB.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loren
+ * Date: 5/15/2019
+ * Time: 11:42 AM
+ */
+
+class DB{
+    var $dbhost = "localhost";
+    var $dbuser = "root";
+    var $dbpass = "";
+    var $dbname = "lessenphp";
+
+    public function makeConnection()
+    {
+        $obj = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+        return $obj;
+    }
+}
+
+?>
