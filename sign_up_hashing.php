cd36f9f15590482a376961c9c05d53ff29593d27
Luca Stronks <lucastronks@gmail.com>
2019-05-23 11:19:06 +0200

    Added more textfields and finished database connection
    All user information can now be entered and will be put into the database.

--- sign_up_hashing.php
+++ sign_up_hashing.php
@@ -13,19 +13,56 @@
         <h1>Registreren</h1>
         <form action="#" method="post">
             <div class="form-group">
-                <label for="gebruikersnaam">Gebruikersnaam</label>
-                <input type="text" class="form-control" name="gebruikersnaam" placeholder="Gebruikersnaam">
-
-                <!--
-                Username: <input type="text" name="username"><br>
-                Password: <input type="text" name="password"><br>
-                <input type="submit" value="Submit">
-                -->
+                <label for="username">Gebruikersnaam</label>
+                <input type="text" class="form-control" name="username" placeholder="Gebruikersnaam">
             </div>
             <div class="form-group">
-                <label for="wachtwoord">Wachtwoord</label>
-                <input type="password" class="form-control" name="wachtwoord" placeholder="Wachtwoord">
-
+                <label for="email">Email</label>
+                <input type="email" class="form-control" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <label for="passowrd">Wachtwoord</label>
+                <input type="password" class="form-control" name="password" placeholder="Wachtwoord">
+            </div>
+            <div class="form-group">
+                <label for="firstname">Voornaam</label>
+                <input type="text" class="form-control" name="firstname" placeholder="Voornaam">
+            </div>
+            <div class="form-group">
+                <label for="infix">Tussenvoegsel</label>
+                <input type="text" class="form-control" name="infix" placeholder="Tussenvoegsel">
+            </div>
+            <div class="form-group">
+                <label for="lastname">Achternaam</label>
+                <input type="text" class="form-control" name="lastname" placeholder="Achternaam">
+            </div>
+            <div class="form-group">
+                <label for="city">Plaats</label>
+                <input type="text" class="form-control" name="city" placeholder="Plaats">
+            </div>
+            <div class="form-group">
+                <label for="street">Straat</label>
+                <input type="text" class="form-control" name="street" placeholder="Straat">
+            </div>
+            <div class="form-group">
+                <label for="housenumber">Huisnummer</label>
+                <input type="text" class="form-control" name="housenumber" placeholder="Huisnummer">
+            </div>
+            <div class="form-group">
+                <label for="postalcode">Postcode</label>
+                <input type="text" class="form-control" name="postalcode" placeholder="Postcode">
+            </div>
+            <div class="form-group">
+                <label for="telephone">Telefoonnummer</label>
+                <input type="text" class="form-control" name="telephone" placeholder="Telefoonnummer">
+            </div>
+            <div class="form-group">
+                <label for="gender">Geslacht</label>
+                <input type="text" class="form-control" name="gender" placeholder="Geslacht">
+            </div>
+            <div class="form-group">
+                <label for="age">Leeftijd</label>
+                <input type="text" class="form-control" name="age" placeholder="Leeftijd">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
@@ -49,11 +86,24 @@ if($_POST != NULL)
     echo "<b>Username: </b>" . $_POST['username'] . "<br>";
     echo "<b>Password SHA256: </b>" . $hash . "<br>";
 
-    $inUsername = $_POST['username'];
+    // Setting of variables
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $firstname = $_POST['firstname'];
+    $infix = $_POST['infix'];
+    $lastname = $_POST['lastname'];
+    $city = $_POST['city'];
+    $street = $_POST['street'];
+    $housenumber = $_POST['housenumber'];
+    $postalcode = $_POST['postalcode'];
+    $telephone = $_POST['telephone'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+
 
 
     // Use DBAL to send data
-    DatabaseAction::sendUserData("INSERT INTO users (userUsername, userPasswordHash, userID) VALUES ('$inUsername', '$hash', NULL)");
+    DatabaseAction::sendUserData("INSERT INTO users (userID, userFirstName, userInfix, userLastName, userEmailAddress, userPasswordHash, userHouseNumber, userStreet, userCity, userPhoneNumber, userGender, userPostalCode, userAge, userPurchases, userUsername) VALUES (NULL, '$firstname', '$infix', '$lastname', '$email', '$hash', '$housenumber', '$street', '$city', '$phonphone', '$gender', '$postalcode', '$age', '$username')");
 
     //DatabaseAction::getUserData("SELECT * FROM users");
 }
